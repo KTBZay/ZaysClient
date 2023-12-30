8e03d40c92805a5208ac6b31f2b1ca80520ea5e9
KTBZay <97396492+KTBZay@users.noreply.github.com>
2023-12-30 22:47:40 +0000

    Created new exsamples 	new file:   exsamples/create_request/index.js 	new file:   exsamples/get_request/index.js 	new file:   exsamples/post_request/index.js

---
+++ exsamples/create_request/index.js
@@ -0,0 +1,8 @@
+const {wizard} = require("zaysclient")
+
+wizard.settings.enableAllLogs = true;
+
+const client = new wizard.ZaysClient();
+
+client.create(id,token);
+
---
+++ exsamples/post_request/index.js
@@ -0,0 +1,7 @@
+const {wizard} = require("zaysclient")
+
+wizard.settings.enableAllLogs = true;
+
+const client = new wizard.ZaysClient();
+
+client.post(url, {form:{}});
