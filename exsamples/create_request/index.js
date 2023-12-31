a68cc73a4284b290a0dd759e1fb8e345b4211a89
KTBZay <97396492+KTBZay@users.noreply.github.com>
2023-12-30 19:27:09 -0500

    Update index.js

--- exsamples/create_request/index.js
+++ exsamples/create_request/index.js
@@ -1,8 +1,8 @@
-const {wizard} = require("zaysclient")
+const {ZayClient} = require("zaysclient")
 
-wizard.settings.enableAllLogs = true;
-
-const client = new wizard.ZaysClient();
-
+const client = new ZayClient({
+    enableAllLogs: true,
+    DataUploading: false,
+    leaveProcessOpen: false
+});  
 client.create(id,token);
-
